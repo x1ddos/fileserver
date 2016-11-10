38d7212506ec6c8c357e00055be4348c065a21af
Alex Vaghin <ddos@google.com>
2016-11-10 11:41:19 +0100

    Allow multiple pattern:dir arguments
    Also, log incoming requests.

--- README.txt
+++ README.txt
@@ -1,10 +1,17 @@
 Simplest file server to (temporarily) serve static content from a local filesystem.
 
-go get github.com/x1ddos/fileserver
-fileserver -d /tmp -a 0.0.0.0 -p 8001
+Usage:
 
-Usage of fileserver:
-  -a="localhost": Address to listen on
-  -d=".": Root directory
-  -p=8000: Port to listen at
- 
+    fileserver [-a host:port] [pattern:]dir ...
+
+The program serves files from a local dist using dir argument as root.
+An optional pattern specified the URL base path. For instance,
+to serve /tmp dir at /temporary, one can use the following:
+
+    fileserver /temporary:/tmp
+
+The dir argument can be specified multiple times.
+If no dir argument is provided, current directory is used.
+
+  -a string
+    	Address (host:port) to listen on (default "localhost:8000")
--- fileserver.go
+++ fileserver.go
@@ -2,27 +2,58 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
-var (
-	port    *int    = flag.Int("p", 8000, "Port to listen at")
-	address *string = flag.String("a", "localhost", "Address to listen on")
-	root    *string = flag.String("d", ".", "Root directory")
-)
+const usageText = `Usage: fileserver [-a host:port] [pattern:]dir ...
+
+The program serves files from a local dist using dir argument as root.
+An optional pattern specified the URL base path. For instance,
+to serve /tmp dir at /temporary, one can use the following:
+
+    fileserver /temporary:/tmp
+
+The dir argument can be specified multiple times.
+If no dir argument is provided, current directory is used.
+
+`
+
+var address *string = flag.String("a", "localhost:8000", "Address (host:port) to listen on")
 
 func main() {
-	flag.Parse()
-
-	httproot := http.Dir(*root)
-	*address = fmt.Sprintf("%s:%d", *address, *port)
-	fmt.Printf("Serving %s on http://%s\n", httproot, *address)
-
-	http.Handle("/", http.FileServer(httproot))
-	err := http.ListenAndServe(*address, nil)
-	if err != nil {
-		log.Fatal(err)
+	flag.Usage = func() {
+		os.Stderr.WriteString(usageText)
+		flag.PrintDefaults()
 	}
+	flag.Parse()
+	roots := flag.Args()
+	if len(roots) == 0 {
+		roots = append(roots, ".")
+	}
+	for _, r := range roots {
+		var p string
+		if i := strings.IndexRune(r, ':'); i >= 0 {
+			p = r[:i]
+			r = r[i+1:]
+		}
+		if !strings.HasPrefix(p, "/") {
+			p = "/" + p
+		}
+		fs := http.FileServer(http.Dir(r))
+		if len(p) > 1 {
+			fs = http.StripPrefix(p, fs)
+		}
+		http.Handle(p, fs)
+	}
+
+	log.Printf("serving %s on http://%s", roots, *address)
+	log.Fatal(http.ListenAndServe(*address, http.HandlerFunc(handle)))
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s %s", r.Method, r.RequestURI)
+	http.DefaultServeMux.ServeHTTP(w, r)
 }
